ef846924b548cbe927dfb313a03b0198e72b2a72
catalindumitrache76 <catalindumitrache76@yahoo.es>
2016-11-16 15:47:08 +0100

    Completed serviceIsAlive test

--- src/test/java/rest/addressbook/AddressBookServiceTest.java
+++ src/test/java/rest/addressbook/AddressBookServiceTest.java
@@ -7,11 +7,17 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Configuration;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
@@ -23,7 +29,6 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * A simple test suite
- *
  */
 public class AddressBookServiceTest {
 
@@ -31,29 +36,177 @@ public class AddressBookServiceTest {
 
 	@Test
 	public void serviceIsAlive() throws IOException {
+		
 		// Prepare server
 		AddressBook ab = new AddressBook();
 		launchServer(ab);
 
 		// Request the address book
 		Client client = ClientBuilder.newClient();
-		Response response = client.target("http://localhost:8282/contacts")
-				.request().get();
-		assertEquals(200, response.getStatus());
-		assertEquals(0, response.readEntity(AddressBook.class).getPersonList()
-				.size());
-
+		Response response1 = client.target("http://localhost:8080/contacts").
+				request().get();
+		Response response2 = client.target("http://localhost:8080/contacts").
+				request().get();
+		Response response3 = client.target("http://localhost:8080/contacts").
+				request().get();
+		Response response4 = client.target("http://localhost:8080/contacts").
+				request().get();
+		boolean entityBuffer = response1.bufferEntity();
+		AddressBook adEntity = response1.readEntity(AddressBook.class);
+		List<Person> people = adEntity.getPersonList();
 		
-		// Must be extended in order to verify that GET /contacts meets the HTTP 
-		// contract: it is safe (it does not modify the contents of the address 
-		// book) and idempotent (two consecutive calls returns the same).
+		// Check that it returns 200 and the data is correct.
+		assertEquals(200, response1.getStatus());
+		assertEquals(2, people.size());
+		Iterator<Person> itPeople = people.iterator();
+		Person p1 = itPeople.next();
+		Person p2 = itPeople.next();
+		assertEquals(1,p1.getId());
+		assertEquals("Salvador",p1.getName());
+		assertEquals(2,p2.getId());
+		assertEquals("Juan",p2.getName());	
+		
+		System.out.println("Entity buffer:");
+		System.out.println(entityBuffer);
+		System.out.println("------------------------------------------------" + 
+				"------------------");
+		
+		/**
+		 * Checking idempotency for 3 get requests
+		 */
+		List<Person> people1 = people(response2);
+		List<Person> people2 = people(response3);
+		List<Person> people3 = people(response4);
+		
+		assertEquals(people1.size(),people2.size());
+		assertEquals(people1.size(),people3.size());
+		
+		Iterator<Person> people1Iterator = people1.iterator();
+		Iterator<Person> people2Iterator = people2.iterator();
+		Iterator<Person> people3Iterator = people3.iterator();
+		
+		for (int i = 0; i < people1.size(); i++){
+			Person person1 = people1Iterator.next();
+			Person person2 = people2Iterator.next();
+			Person person3 = people3Iterator.next();
+			assertEquals(person1.getId(),person2.getId());
+			assertEquals(person1.getName(),person2.getName());
+			assertEquals(person1.getId(),person3.getId());
+			assertEquals(person1.getName(),person3.getName());
+		}
+		
+		/**
+		 *  These parts are extras. I was just checking them out
+		 */
+		Configuration config = client.target("http://localhost:8080/contacts").
+				getConfiguration();
+		Configuration clientConfig = client.getConfiguration();
+		
+		System.out.println("A continuación imprimo las propiedades" + 
+				" de localhost:8080/contacts:");
+		Map<String, Object> configMap = config.getProperties();
+		Iterator<String> configKeyIterator = configMap.keySet().iterator();
+		String ckey = "";
+		Object cvalue = null;
+		while (configKeyIterator.hasNext()){
+			ckey = configKeyIterator.next();
+			cvalue = configMap.get(ckey);
+			System.out.println(ckey + ": " + cvalue);
+		}
+		System.out.println("------------------------------------------------" + 
+				"------------------");
+		
+		
+		System.out.println("A continuación imprimo las propiedades del "
+				+ "cliente:");
+		Map<String, Object> configClientMap = clientConfig.getProperties();
+		Iterator<String> configClientKeyIterator = configClientMap.keySet().
+				iterator();
+		String cckey = "";
+		Object ccvalue = null;
+		while (configClientKeyIterator.hasNext()){
+			cckey = configClientKeyIterator.next();
+			ccvalue = configClientMap.get(cckey);
+			System.out.println(cckey + ": " + ccvalue);
+		}
+		System.out.println("------------------------------------------------" + 
+				"------------------");
+		
+		
+		System.out.println("A continuación imprimo los nombres de las "
+				+ "propiedades de localhost:8080/contacts:");
+		Collection<String> configNames = config.getPropertyNames();
+		Iterator<String> configNameKeyIterator = configNames.iterator();
+		String cNkey = "";
+		while (configNameKeyIterator.hasNext()){
+			cNkey = configNameKeyIterator.next();
+			System.out.println(cNkey + ": " + cvalue);
+		}
+		System.out.println("------------------------------------------------" + 
+				"------------------");
+		
+		
+		System.out.println("A continuación tengo la cabecera:");
+		MultivaluedMap<String, Object> headers = response1.getHeaders();
+		Iterator<String> headerKeyIterator = headers.keySet().iterator();
+		String hkey = "";
+		Object hvalue = null;
+		while (headerKeyIterator.hasNext()){
+			hkey = headerKeyIterator.next();
+			hvalue = headers.get(hkey);
+			System.out.println(hkey + ": " + hvalue);
+		}
+		System.out.println("------------------------------------------------" + 
+				"------------------");
+		
+		
+		System.out.println("A continuación tengo la fecha de respuesta:");
+		System.out.println(response1.getDate());
+		System.out.println("------------------------------------------------" + 
+				"------------------");
+		
+		
+		System.out.println("A continuación tengo los métodos HTTP" + 
+		" permitidos sobre el response:");
+		Iterator<String> allowedMethodsKeyIterator = response1.
+				getAllowedMethods().iterator();
+		String aMkey = "";
+		Object aMvalue = null;
+		while (allowedMethodsKeyIterator.hasNext()){
+			aMkey = allowedMethodsKeyIterator.next();
+			aMvalue = headers.get(aMkey);
+			System.out.println(aMkey + ": " + aMvalue);
+		}
+		System.out.println("------------------------------------------------" + 
+				"------------------");
+		
+		
+		System.out.println("A continuación tengo la location del response:");
+		System.out.println(response1.getLocation());
+		System.out.println("------------------------------------------------" + 
+				"------------------");
+		
+		
+		System.out.println("A continuación tengo el status info del response:");
+		System.out.println(response1.getStatusInfo());
+		System.out.println("------------------------------------------------" + 
+				"------------------");
+		
 		
 		//////////////////////////////////////////////////////////////////////
-		// Verify that GET /contacts is well implemented by the service, i.e
-		// test that it is safe and idempotent
+		// Test that GET /contacts is safe and idempotent
 		//////////////////////////////////////////////////////////////////////	
 	}
-
+	
+	
+	private List<Person> people(Response response) {
+		assertEquals(200, response.getStatus());
+		AddressBook adEntity = response.readEntity(AddressBook.class);
+		List<Person> people = adEntity.getPersonList();
+		return people;
+	}
+	
+	
 	@Test
 	public void createUser() throws IOException {
 		// Prepare server
